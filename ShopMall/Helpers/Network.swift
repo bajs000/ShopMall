2cc08b9908ef181b99142d632e0b33c5cef8b4ff
chenguo <bajs000@163.com>
2017-10-16 17:29:15 +0800

    commit

---
+++ ShopMall/Helpers/Network.swift
@@ -0,0 +1,13 @@
+//
+//  Network.swift
+//  ShopMall
+//
+//  Created by YunTu on 2017/10/16.
+//  Copyright © 2017年 YunTu. All rights reserved.
+//
+
+import UIKit
+
+class Network: NSObject {
+
+}
---
+++ ShopMall/Helpers/ShopMall-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  ShopMall-Bridging-Header.h
+//  ShopMall
+//
+//  Created by YunTu on 2017/10/16.
+//  Copyright © 2017年 YunTu. All rights reserved.
+//
+
+#ifndef ShopMall_Bridging_Header_h
+#define ShopMall_Bridging_Header_h
+
+
+#endif /* ShopMall_Bridging_Header_h */
